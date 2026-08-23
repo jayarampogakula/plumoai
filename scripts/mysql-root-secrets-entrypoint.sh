eced350d6f89271c790222440ed3527e1b56ee5b
Jayaram <jayarampogakula@gmail.com>
2026-08-23 20:41:05 +0530

    Use native official entrypoints for MySQL and MongoDB and normalize LF line endings

--- scripts/mysql-root-secrets-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Set MySQL root password from secret (separate from app user).
-set -e
-if [ -f /run/secrets/mysql_root_password ]; then
-  export MYSQL_ROOT_PASSWORD="$(cat /run/secrets/mysql_root_password)"
-fi
-exec /usr/local/bin/docker-entrypoint.sh "$@"
